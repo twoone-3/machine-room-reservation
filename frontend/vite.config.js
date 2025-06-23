b8d66854df948bf6d9e0f4bf8f2a8b98a75c43d3
TunL <3164280353@qq.com>
2025-06-23 17:29:45 +0800

    据说能解决热重载？

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    watch: {
+      // 告诉 vite 不要监视后端文件夹的变化
+      ignored: ['!**/node_modules/**', '../backend/**']
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
